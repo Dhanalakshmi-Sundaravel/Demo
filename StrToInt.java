7d86ca96b9967ba1aaec355a34faec88597019f9
Dhanalakshmi S <Dhanalakshmi S>
2016-08-23 13:07:38 +0530

    Convert String to Integer

---
+++ StrToInt.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class StrToInt{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		int n=Integer.parseInt(str);
+		System.out.println(n);
+	}
+}
